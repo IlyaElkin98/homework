76a126217ea9c4bf2ce2b0417e2f6a8516adccb4
ilya <ilifud98@mail.ru>
2025-02-27 20:15:07 +0700

    add test_widget and rework widget

--- src/widget.py
+++ src/widget.py
@@ -6,6 +6,8 @@ def get_mask_account(score_or_card: str) -> str:
     score = "Счет"
     if score in score_or_card:
         return f"Счет **{score_or_card[-4:]}"
+    elif len(score_or_card) == 0:
+        return "Пустая строка"
     else:
         list_number_card = score_or_card.split()
         name_card = ""
@@ -15,11 +17,17 @@ def get_mask_account(score_or_card: str) -> str:
                 name_card += i
             elif i.isdigit():
                 number_card += i
-                return f"{(name_card)} {number_card[:4]} {number_card[6:8]}** **** {number_card[-4:]}"
+                return f"{(name_card)} {number_card[:4]} {number_card[4:6]}** **** {number_card[-4:]}"
 
 
 def get_date(date_time: str) -> str:
     """Функция, которая принимает на вход строку с датой и временем и возвращает строку с датой"""
+    if len(date_time) == 0:
+        return "Пустая строка!"
+    elif date_time[2] == '-':
+        return f"{date_time[:2]}.{date_time[3:5]}.{date_time[6:10]}"
+    elif '-' not in date_time:
+        return "Во входной строке отсутствует дата"
     return f"{date_time[8:10]}.{date_time[5:7]}.{date_time[:4]}"
 
 
