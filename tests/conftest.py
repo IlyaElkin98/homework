c7cb9887fdfd2f7a16f5617f192d1bb6e93c3338
ilya <ilifud98@mail.ru>
2025-02-28 20:34:42 +0700

    итог

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,20 +2,20 @@ import pytest
 
 
 @pytest.fixture
-def date_1(time: str) -> str:
+def date_1():
     return "11.03.2024"
 
 
 @pytest.fixture
-def date_2(time: str) -> str:
+def date_2():
     return "Пустая строка!"
 
 
 @pytest.fixture
-def date_3(time: str) -> str:
+def date_3():
     return "21.11.2023"
 
 
 @pytest.fixture
-def date_4(time: str) -> str:
+def date_4():
     return "Во входной строке отсутсвует дата"
