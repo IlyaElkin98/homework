aeeedbb52ba640cded108961d8216f15f7e482c5
ilya <ilifud98@mail.ru>
2025-03-04 17:14:11 +0700

    add masks

---
+++ tests/test_masks.py
@@ -0,0 +1,27 @@
+import pytest
+
+from src.masks import get_mask_account, get_mask_card_number
+
+
+@pytest.mark.parametrize(
+    "number_card, masked_number",
+    [
+        ("2200240824255657", "2200 24** **** 5657"),
+        ("220224245454820761", "Неправильно набран номер карты. Проверьте правильность ввода"),
+        ("", "Пустая строка"),
+    ],
+)
+def test_get_mask_card_number(number_card: str, masked_number: str) -> None:
+    assert get_mask_card_number(number_card) == masked_number
+
+
+@pytest.mark.parametrize(
+    "account_mask, masked_account",
+    [
+        ("12345678901234567890", "**7890"),
+        ("12313545643879504078434", "Неправильно набран номер счета. Проверьте правильность ввода"),
+        ("", "Пустая строка"),
+    ],
+)
+def test_get_mask_account(account_mask: str, masked_account: str) -> None:
+    assert get_mask_account(account_mask) == masked_account
